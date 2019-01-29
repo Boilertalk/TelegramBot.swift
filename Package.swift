f1738ddd45c9eea703459c6e11cff7f9a40176cb
Koray Koska <koray@koska.at>
2019-01-29 01:41:44 +0100

    Add optional vapor library

--- Package.swift
+++ Package.swift
@@ -7,14 +7,21 @@ let package = Package(
         .library(
             name: "TelegramBot",
             targets: ["TelegramBot"]),
+        .library(
+            name: "TelegramBotVapor",
+            targets: ["TelegramBotVapor"]),
     ],
     dependencies: [
         // Test dependencies
         .package(url: "https://github.com/Quick/Quick.git", from: "1.2.0"),
-        .package(url: "https://github.com/Quick/Nimble.git", from: "7.0.3")
+        .package(url: "https://github.com/Quick/Nimble.git", from: "7.0.3"),
+
+        // Vapor for vapor related requests, optional.
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
     ],
     targets: [
         .target(name: "TelegramBot", dependencies: []),
+        .target(name: "TelegramBotVapor", dependencies: ["TelegramBot", "Vapor"]),
         .testTarget(name: "TelegramBotTests", dependencies: ["TelegramBot", "Quick", "Nimble"])
     ]
 )
