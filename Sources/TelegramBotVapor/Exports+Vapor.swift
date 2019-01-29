5326db7a1f649e4217620d4479ea52749320622b
Koray Koska <koray@koska.at>
2019-01-29 14:08:30 +0100

    Move default vapor implementation

---
+++ Sources/TelegramBotVapor/Exports+Vapor.swift
@@ -0,0 +1,37 @@
+// MARK: - Default Vapor implementation
+
+import TelegramBot
+import Vapor
+
+extension Router {
+
+    public func telegramRegister(path: String, callback: @escaping (_ req: TelegramHTTPRequest, _ cb: @escaping (TelegramHTTPStatus) -> ()) throws -> ()) {
+        post(path) { reqInternal -> Future<HTTPStatus> in
+            let promiseStatus = reqInternal.eventLoop.newPromise(HTTPStatus.self)
+
+            try callback(reqInternal) { status in
+                switch status {
+                case .ok:
+                    promiseStatus.succeed(result: .ok)
+                case .badRequest:
+                    promiseStatus.succeed(result: .badRequest)
+                case .internalServerError:
+                    promiseStatus.succeed(result: .internalServerError)
+                }
+            }
+
+            return promiseStatus.futureResult
+        }
+    }
+}
+
+extension Request: TelegramHTTPRequest {
+
+    public func json<Type: Decodable>(_ type: Type.Type, decoder: JSONDecoder, callback: @escaping (Type) -> ()) throws {
+        _ = try content.decode(json: Type.self, using: decoder).map { decoded -> () in
+            callback(decoded)
+
+            return ()
+        }
+    }
+}
